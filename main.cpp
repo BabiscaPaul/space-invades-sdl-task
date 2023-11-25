60e74884024d12c99964957f09bb4fdf6ff08073
Iosua Paul <babisca.paul1@gmail.com>
2023-11-25 12:02:01 +0200

    added moving canon

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
+const int SQUARE_SIZE = 50;
+
+int squareX = (WINDOW_WIDTH - SQUARE_SIZE) / 2;
+int squareY = WINDOW_HEIGHT - SQUARE_SIZE;
+
+
 //define SDL Window related variables
-SDL_Window* window = NULL;
-SDL_Renderer* windowRenderer = NULL;
+SDL_Window *window = NULL;
+SDL_Renderer *windowRenderer = NULL;
 SDL_Event currentEvent;
 
 bool quit = false;
@@ -24,8 +30,7 @@ bool initWindow() {
         std::cout << "SDL initialization failed" << std::endl;
         success = false;
 
-    }
-    else {
+    } else {
 
         //Try to create the window
         window = SDL_CreateWindow(
@@ -98,32 +103,24 @@ void processEvents() {
 
     //Keyboard event
     if (currentEvent.type == SDL_KEYDOWN) {
-
         switch (currentEvent.key.keysym.sym) {
-
             case SDLK_UP:
                 break;
-
             case SDLK_DOWN:
                 break;
-
             case SDLK_LEFT:
-                break;
-
+                squareX -= 20;
+                break;  // Add break to exit the switch block after moving left
             case SDLK_RIGHT:
-                break;
-
+                squareX += 20;
+                break;  // Add break to exit the switch block after moving right
             case SDLK_r:
                 break;
-
             case SDLK_s:
                 break;
-
             case SDLK_ESCAPE:
-
                 quit = true;
                 break;
-
             default:
                 break;
         }
@@ -140,6 +137,25 @@ void drawFrame() {
     //TODO
 
     //Update window
+    SDL_RenderPresent(windowRenderer);
+
+    //draw the canon (black) and position it correctly
+
+    // Clear the background
+    SDL_SetRenderDrawColor(windowRenderer, 255, 255, 255, 255);
+    SDL_RenderClear(windowRenderer);
+
+    // Draw the square at the updated position
+    SDL_Rect squareRect;
+    squareRect.w = SQUARE_SIZE;
+    squareRect.h = SQUARE_SIZE;
+    squareRect.x = squareX;
+    squareRect.y = squareY;
+
+    SDL_SetRenderDrawColor(windowRenderer, 0, 0, 0, 255); // Set color to black
+    SDL_RenderFillRect(windowRenderer, &squareRect);
+
+    // Update window
     SDL_RenderPresent(windowRenderer);
 }
 
@@ -163,7 +179,7 @@ void cleanup() {
     SDL_Quit();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
     //Initialize window
     if (!initWindow()) {
